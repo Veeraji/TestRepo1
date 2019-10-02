eec83e6870460c616ecc3f8611d2c27b6d94dc0a
Veeraji <56072314+Veeraji@users.noreply.github.com>
2019-10-02 16:26:56 +0530

    Add files via upload
    init

---
+++ a2.py
@@ -0,0 +1,14 @@
+num1 = 1.0 / 2.0
+num2 = 3.0 / 4.0
+sum = num1 + num2
+print(sum)
+'''
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("http://google.com")
+#driver.close()
+'''
+'''ffsdfds
+fdsfdsf
+'''
+
